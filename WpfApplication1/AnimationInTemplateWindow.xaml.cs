0fc89dd99d956a32f70f48b5ac80b7af8f4e0e1d
Changweihua <changweihua@outlook.com>
2013-08-28 09:48:40 +0800

    创建项目

---
+++ WpfApplication1/AnimationInTemplateWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Collections.ObjectModel;
+using System.Collections;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// AnimationInTemplateWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class AnimationInTemplateWindow : Window
+    {
+        public AnimationInTemplateWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Dispatcher.BeginInvoke((ThreadStart)delegate(){
+                lixtBox1.ItemsSource = new ObservableCollection<int>(Enumerable.Range(1, 10));
+            });
+        }
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation widthAnimation = new DoubleAnimation();
+            widthAnimation.From = button1.ActualWidth;
+            widthAnimation.To = this.Width - 30;
+            widthAnimation.Duration = TimeSpan.FromSeconds(5);
+            widthAnimation.DecelerationRatio = 0.7;
+            button1.BeginAnimation(Button.WidthProperty, widthAnimation);
+        }
+    }
+}
